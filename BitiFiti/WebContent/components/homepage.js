e1785e18ebbabab6376f3024b8776f7af668f00a
Đorđe <the2922000@gmail.com>
2022-06-15 01:58:53 +0200

    Login screen

--- BitiFiti/WebContent/components/homepage.js
+++ BitiFiti/WebContent/components/homepage.js
@@ -3,17 +3,64 @@ Vue.component("homepage", {
 	// podaci
 	data: function () {
 	    return {
-	      products: null
+	        products: null
 	    }
 	},
 	// html bootstrap
 	    template: ` 
-    	<div>
-    		<h3>Ovo je pocetni ekran</h3>
-    		<button v-on:click = "login">Login Ekran</button>
-    	</div>		  
-    	`,
-	// n apocetku
+<div>
+
+	<!-- Navigation-->
+            <nav class="navbar navbar-expand-lg navbar-dark navbar-custom text-bg-dark">
+                <div class="container px-5">
+                    <a class="navbar-brand" href="http://localhost:8080/BitiFiti/#">
+                        <img src="" alt="" width="30" height="24" class="d-inline-block align-text-top">
+                        BitiFiti
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                    <div class="collapse navbar-collapse" id="navbarResponsive">
+                        <ul class="navbar-nav ms-auto">
+                            <li class="nav-item"><a class="nav-link" href="#!">Sign Up</a></li>
+                            <li class="nav-item"><a class="nav-link" href="http://localhost:8080/BitiFiti/#/login">Log In</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+    <!-- Header-->
+            <header class="masthead text-center text-black">
+                <div class="masthead-content">
+                    <div class="container px-5">
+                        <h1 class="masthead-heading mb-0">One Page Wonder</h1>
+                        <h2 class="masthead-subheading mb-0">Will Rock Your Socks Off</h2>
+                        <a class="btn btn-primary btn-xl rounded-pill mt-5" href="#scroll">Learn More</a>
+                    </div>
+                </div>
+            </header>
+
+    <!-- Content section 1-->
+            <section id="scroll">
+                <div class="container px-5">
+                    <div class="row gx-5 align-items-center">
+                        <div class="col-lg-6 order-lg-2">
+                            <div class="p-5"><img class="img-fluid rounded-circle" src="assets/img/01.jpg" alt="..." /></div>
+                        </div>
+                        <div class="col-lg-6 order-lg-1">
+                            <div class="p-5">
+                                <h2 class="display-4">For those about to rock...</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+    <!-- Footer-->
+            <footer class="py-5 bg-black">
+                <div class="container px-5"><p class="m-0 text-center text-white small">Copyright &copy; Đorđe & Boško doo</p></div>
+            </footer>
+</div>`,
+	// na pocetku
     mounted () {
         axios
           .get('rest/products/')
--- BitiFiti/WebContent/components/login.js
+++ BitiFiti/WebContent/components/login.js
@@ -1,32 +1,125 @@
 Vue.component("login", { 
-	data: function () {
+    data: function () {
 	    return {
-	      products: null
+	        user: {
+				username: '',
+                password: '',
+                name: '',
+                surname: '',
+                gender: 0,
+                birthDate: '',
+                role: 0,
+                deleted: false,
+                blocked: false
+		  	},
+            greska: "",
 	    }
 	},
-	    template: ` 
-    	<div>
-    		<h3>Ovo je login ekran</h3>
-        <form id="forma">
-            <table>
-                <tr><td>Username</td><td><input type="text" name="username"></td></tr>
-                <tr><td>Password</td><td><input type="password" name="password"></td></tr>
-                <tr><td><input type="submit" value="Login"></td></tr>
-            </table>
-            <p id="error" hidden="true"></p>
-            <p id="success" hidden="true"></p>
-	    </form>
-    	<button v-on:click = "login">pocetni Ekran</button>
-    	</div>		  
-    	`,
+	    template: `<div>
+
+
+    <!-- Navigation-->
+            <nav class="navbar navbar-expand-lg navbar-dark navbar-custom text-bg-dark">
+                <div class="container px-5">
+                    <a class="navbar-brand" href="http://localhost:8080/BitiFiti/#/">
+                        <img src="" alt="" width="30" height="24" class="d-inline-block align-text-top">
+                        BitiFiti
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                    <div class="collapse navbar-collapse" id="navbarResponsive">
+                        <ul class="navbar-nav ms-auto">
+                            <li class="nav-item"><a class="nav-link" href="http://localhost:8080/BitiFiti/#/signin">Sign Up</a></li>
+                            <li class="nav-item"><a class="nav-link" href="http://localhost:8080/BitiFiti/#/login">Log In</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        
+
+            <section class="vh-100" style="background-color: #f4a552;">
+            <div class="container py-5 h-100">
+                <div class="row d-flex justify-content-center align-items-center h-100">
+                    <div class="col col-xl-10">
+                        <div class="card" style="border-radius: 1rem;">
+                            <div class="row g-0">
+                                <div class="col-md-6 col-lg-5 d-none d-md-block">
+                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
+                                    alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
+                                </div>
+                                <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                                    <div class="card-body p-4 p-lg-5 text-black">
+        
+                                            <div class="d-flex align-items-center mb-3 pb-1">
+                                                <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
+                                                <span class="h1 fw-bold mb-0">Logo</span>
+                                            </div>
+        
+                                            <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Prijavite se</h5>
+        
+                                            <div class="form-outline mb-4">
+                                                <input type="email" class="form-control form-control-lg" />
+                                                <label class="form-label">Korisničko ime</label>
+                                            </div>
+        
+                                            <div class="form-outline mb-4">
+                                                <input type="password" class="form-control form-control-lg" />
+                                                <label class="form-label">Šifra</label>
+                                            </div>
+
+                                            <div style="color: red;" id="greska">{{greska}}</div>
+
+                                            <div class="pt-1 mb-4">
+                                                <button @click="checkUser()" class="btn btn-dark btn-lg btn-block" type="button">Prijava</button>
+                                            </div>
+                                            <p class="mb-5 pb-lg-2" style="color: #393f81;">Nemate nalog? <a href="http://localhost:8080/BitiFiti/#/signin"
+                                            style="color: #393f81;">Registujte se</a></p>
+        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    <!-- Footer-->
+            <footer class="py-5 bg-black">
+                <div class="container px-5"><p class="m-0 text-center text-white small">Copyright &copy; Đorđe & Boško doo</p></div>
+            </footer>
+        
+
+
+</div>`,
     mounted () {/*
         axios
-          .get('rest/products/')
-          .then(response => (this.products = response.data))*/
+            .get('rest/products/')
+            .then(response => (this.products = response.data))*/
     },
     methods: {
-    	login : function() {
-    		this.$router.push("/")	
-    	}
+        checkUser: function() {
+            axios
+            .post('/FitiBiti/rest/users/login', this.user)
+            .then(response=> {
+                if(response.data.blocked === false){
+                    if(response.data.role=='ADMINISTRATOR'){
+                        this.$router.push("/homeLoginAdministrator/" + this.user.username);
+                    } else if(response.data.role=='MANAGER'){
+                        this.$router.push("/homeLoginManager/" + this.user.username);
+                    }else if(response.data.role=='TRAINER'){
+                        this.$router.push("/homeLoginTrainer/" + this.user.username);
+                    }else {
+                        this.$router.push("/homeLoginBuyer/" + this.user.username);
+                    }
+                }else if(response.data.blocked === true) {
+                    this.greska="Your account is blocked!"
+                }else{
+                    this.greska="Wrong password or username!"
+                }
+            })
+            .catch(err => {
+                this.greska = "Wrong password or username!";
+            })
+        }
     }
 });
