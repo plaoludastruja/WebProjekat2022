5a6a3ba4d6f1908a1c44e09e82907d88c74f0cff
Bosko Ristovic <malibolence@gmail.com>
2022-06-17 21:15:34 +0200

    add SportObjectDAO

--- BitiFiti/src/dao/SportObjectDAO.java
+++ BitiFiti/src/dao/SportObjectDAO.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -24,11 +25,59 @@ public class SportObjectDAO {
 		}
 	}
 	
+	public List<SportObject> getAll(){
+		return sportObjects;
+	}
+	
+	public SportObject getByName(String name) {
+		for(SportObject o : sportObjects) {
+			if(o.getName().equals(name)) {
+				return o;
+			}
+		}
+		return null;
+	}
+	
+	public boolean addSportObject(SportObject o) {
+		if (sportObjects.contains(o))
+			return false;
+		sportObjects.add(o);
+		saveSportObjects();
+		return true;
+	}
+	
+	public boolean deleteSportObject(String name) {
+		for(SportObject o : sportObjects) {
+			if(o.getName().equals(name)) {
+				sportObjects.remove(o);
+				saveSportObjects();
+				return true;
+			}
+		}
+		return false;
+	}
+	
 	public void loadSportObjects() {
 		ObjectMapper mapper = new ObjectMapper();
 		try {
 			sportObjects = new ArrayList<>(Arrays.asList(mapper.readValue(Paths.get(path).toFile(), SportObject[].class)));
 		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void saveSportObjects() {
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			mapper.writeValue(Paths.get(path).toFile(), sportObjects);
+		} catch (JsonGenerationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (JsonMappingException e) {
