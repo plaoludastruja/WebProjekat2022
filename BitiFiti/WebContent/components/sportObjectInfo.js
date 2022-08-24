dfdb5b0bc8db7dd544d1bccd4c231254abec4e1b
Đorđe <the2922000@gmail.com>
2022-08-24 14:34:56 +0200

    Front za prikaz sportskog objekta

--- BitiFiti/WebContent/components/sportObjectInfo.js
+++ BitiFiti/WebContent/components/sportObjectInfo.js
@@ -3,7 +3,7 @@ Vue.component("sportObjectInfo", {
 	// podaci
 	data: function () {
 	    return {
-            id: this.$route.params.name,
+            sportObjectName: this.$route.params.name,
             sortedbyASC: true,
             nameSearch:'',
 		    typeSearch:'',
@@ -18,22 +18,24 @@ Vue.component("sportObjectInfo", {
 	    template: ` 
 <div>
 
-	<!-- Navigation-->
-            <nav class="navbar navbar-expand-lg navbar-dark navbar-custom text-bg-dark">
-                <div class="container px-5">
-                    <a class="navbar-brand" href="http://localhost:8080/BitiFiti/#">
-                        <img src="" alt="" width="30" height="24" class="d-inline-block align-text-top">
-                        BitiFiti - Pregled svih korisnika
+    <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark navbar-custom text-bg-dark">
+            <div class="container px-5">
+                <div>
+                    <a class="navbar-brand" href="http://localhost:8080/BitiFiti/#/">
+                        <img src="components/Resources/muscle.png" alt="logo" width="24" height="24" class="d-inline-block align-text-top">
+                        BitiFiti - {{sportObjectName}}
                     </a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                    <div class="collapse navbar-collapse" id="navbarResponsive">
-                        <ul class="navbar-nav ms-auto">
-                            <li class="mx-5" @click="openHomepage()" class="nav-item">Pocetna</li>
-                        </ul>
-                        <a class="navbar-brand" href="http://localhost:8080/BitiFiti/#">Pocetna</a>
-                    </div>
+                </div> 
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        
+                        <li class="nav-item mx-1" role="button" @click="logOut()">Odjavi se</li>
+                    </ul>
                 </div>
-            </nav>
+            </div>
+        </nav>
 
     <!-- pretraga -->
         <section class="bg-dark">
@@ -47,31 +49,38 @@ Vue.component("sportObjectInfo", {
             </div>
         </section>
 
-        <!-- tabela -->
-        <section id="scroll">
-            <div class="container px-5">
-                <div class="row gx-5 align-items-center">
-                <table class="table">
-                <thead>
-                    <tr>
-                        <th>
-                        <label v-on:click="sortList('name')">Korisnicko ime</label>
-                        </th>
-                        <th v-on:click="sortList('sportObjectType')">Sifra</th>
-                        <th v-on:click="sortList('sportObjectType')">Tip korisnika</th>
-                    </tr>
-                </thead>
-                <tbody v-for="service in this.sportObject.services">
-                    <tr>
-                        <td>{{service.name}}</td>
-                        <td>{{service.serviceType}}</td>
-                        <td>{{service.sportObject}}</td>
-                    </tr>
-                </tbody>
-            </table>
+        <!-- Carousel wrapper -->
+            <section id="carousel">
+                <div
+                    id="carouselMultiItemExample"
+                    class="carousel slide carousel-dark text-center"
+                    data-mdb-ride="carousel">
+
+                    <!-- Inner -->
+                    <div class="carousel-inner py-4">
+                        <!-- Single item -->
+                        <div class="carousel-item active">
+                            <div class="container">
+                                <div class="row">
+                                    <div v-for="service in this.sportObject.services" class="col-lg-4">
+                                        <div class="card">
+                                            <img v-bind:src="service.image" class="mx-auto" width="200"/>
+                                            <div class="card-body">
+                                                <h4 class="card-title">{{service.name}}</h4>
+                                                <h6>{{service.ServiceType}}</h6>
+                                                <h6>Trener<p>{{service.trainer}}<p></h6>
+                                                <h6>Cijena<p>{{service.trainer}}<p></h6>
+                                                <h6>Trajanje: {{service.duration}}</h6>
+                                                <button @click="editService(service.name)" type="button" class="btn btn-outline-dark">Izmijeni</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
 
     <!-- Footer-->
             <footer class="py-5 bg-black">
@@ -100,23 +109,8 @@ Vue.component("sportObjectInfo", {
 		},
         getSportObject: function () {
 			axios
-			.get('rest/sportObjects/'+this.id)
+			.get('rest/sportObjects/' + this.sportObjectName)
 			.then(response=> {this.sportObject=response.data})
 		},
-        openMyProfilePage: function(){
-            this.$router.push("/myProfile/"+this.id)
-        },
-        openAllUsersPage: function(){
-            this.$router.push("/allUsers")
-        },
-        addSportObject: function(){
-            this.$router.push("/addSportObject")
-        },
-        addManager: function(){
-            this.$router.push("/addManager")
-        },
-        addTrainer: function(){
-            this.$router.push("/addTrainer")
-        },
     }
 });
